74e34bbbc3e205d250adbec6cfae3288fd2928a4
Volproil <88111301+Volproil@users.noreply.github.com>
2025-05-31 20:14:02 +0300

    FIX_IT 31.05

--- src/mindustry/content/MyUnits.java
+++ src/mindustry/content/MyUnits.java
@@ -1,13 +1,14 @@
 package mindustry.content;
-
 import mindustry.type.UnitType;
 
 public class MyUnits {
     public static UnitType M113;
+    public static UnitType fpvKamikaze;
     public static UnitType t1ship;
 
     public static void load() {
         t1ship = mindustry.content.units.t1ship.createUnit();
         M113 = mindustry.content.units.M113.createM113();
+        fpvKamikaze = FPVDrone1.createFPVKamikaze();
     }
 }
---
+++ src/mindustry/content/units/fpvdrone1.java
@@ -0,0 +1,74 @@
+package mindustry.content.units;
+
+import arc.graphics.Color;
+import mindustry.type.UnitType;
+import mindustry.gen.UnitEntity;
+import mindustry.type.Weapon;
+import mindustry.content.Fx;
+import mindustry.content.StatusEffects;
+import mindustry.type.unit.UnitClass;
+import mindustry.entities.abilities.StatusFieldAbility;
+import mindustry.entities.bullet.BombBulletType;
+import mindustry.entities.effect.WaveEffect;
+
+public class FPVDrone1 {
+    public static UnitType createFPVKamikaze() {
+        return new UnitType("fpv-kamikaze") {{
+            localizedName = "FPV Drone Kamikadze";
+            speed = 7f;
+            health = 40;
+            hitSize = 1f;
+            flying = true;
+            omniMovement = false;
+            outlineColor = Color.valueOf("2d2f39");
+            layerOffset = -1f;
+            constructor = UnitEntity::create;
+
+            abilities.add(new StatusFieldAbility(StatusEffects.none, 5f, -1f, 0.5f, 2f, true, new WaveEffect() {{
+                followParent = true;
+                rotWithParent = true;
+                useRotation = false;
+                mirror = true;
+                baseRotation = -45f;
+                lifetime = 16f;
+                length = 0f;
+                spin = 1f;
+                region = "military-techic-mod-fpvdrone1-motor-blade";
+                layerOffset = 1f;
+                sizeFrom = 4f;
+                sizeTo = 4f;
+            }}));
+
+            weapons.add(new Weapon() {{
+                shootCone = 140f;
+                mirror = false;
+                shootOnDeath = true;
+                shootEffect = Fx.none;
+                x = 0f;
+                y = 0f;
+                despawnOnHit = true;
+
+                bullet = new BombBulletType() {{
+                    splashDamageRadius = 50f;
+                    splashDamage = 300f;
+                    instantDisappear = true;
+                    killShooter = true;
+                    hittable = false;
+                    collidesAir = true;
+                    status = StatusEffects.melting;
+                    pierce = true;
+
+                    hitEffect = new WaveEffect() {{
+                        lifetime = 35f;
+                        sizeFrom = 20f;
+                        sizeTo = 50f;
+                        strokeFrom = 9f;
+                        strokeTo = 3f;
+                        colorFrom = Color.valueOf("ffca14");
+                        colorTo = Color.valueOf("ff0010");
+                    }};
+                }};
+            }});
+        }};
+    }
+}
