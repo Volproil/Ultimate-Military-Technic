a51a277de2b15987706300f4e9d86efd9d2b6875
Volproil <88111301+Volproil@users.noreply.github.com>
2025-05-31 22:26:07 +0300

    Update MyUnits.java

--- src/mindustry/content/MyUnits.java
+++ src/mindustry/content/MyUnits.java
@@ -20,25 +20,3 @@ public class MyUnits {
 
 
 
-package mindustry.content;
-import mindustry.type.UnitType;
-
-public class MyUnits {
-    public static UnitType M113;
-    public static UnitType fpvdrone1;
-<<<<<<< HEAD
-    public static UnitType t1ship;
-    public static UnitType ATGMVehicle;
-    public static UnitType towrocket;
-=======
-    //public static UnitType t1ship;
-
->>>>>>> 890bd0016f9c133de452768aeb8281e70362ffd2
-    public static void load() {
-        //t1ship = mindustry.content.units.t1ship.createUnit();
-        M113 = mindustry.content.units.M113.createM113();
-        fpvdrone1 = mindustry.content.units.fpvdrone1.createFPVKamikaze();
-        ATGMVehicle = mindustry.content.units.ATGMVehicle.createATGMVehicle();
-        //towrocket = mindustry.content.units.Towrocket.createTowrocket();
-         }
-}
