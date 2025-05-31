48d9385fa9634dfc678a2f2762c78a2b55d6f752
AWATV <111561766+AWATV@users.noreply.github.com>
2025-05-31 19:34:33 +0200

    Ship update

--- src/mindustry/content/units/t1ship.java
+++ src/mindustry/content/units/t1ship.java
@@ -1,39 +1,55 @@
 package mindustry.content.units;
 
-import arc.Core;
 import arc.graphics.Color;
 import mindustry.type.UnitType;
-import mindustry.type.unit.ErekirUnitType;
 import mindustry.gen.UnitEntity;
 import mindustry.type.Weapon;
 import mindustry.entities.bullet.BasicBulletType;
-import mindustry.Vars;
 
 public class t1ship {
     public static UnitType createUnit() {
         return new UnitType("t1ship") {{
-            description = "t1 ship";
-            health = 80;
-            speed = 1.3f;
-            rotateSpeed = 2.6f;
+            description = "t1 ship"; //Localized description & details. May be null.
+            health = 80;             //Unit health
+            speed = 1.5f;            //Unit speed
+            rotateSpeed = 2.6f;      //Unit rotation speed
+            range = 23;             //Unit fire range
+            flying = false;          //If true, the unit can fly
+            rotateMoveFirst = true;  //If true, the unit can rotate before moving
+
             constructor = UnitEntity::create;
 
             weapons.add(new Weapon("umt-java-t1turret") {{
                 x = 0;
-                y = 0;
+                y = -2;
                 rotate = true;
-                top = true;
-                reload = 20f;
-                layerOffset = 3;
+                rotateSpeed = 6;
+                reload = 60;
+                //layerOffset = 1;
+                outlineColor = Color.valueOf("00000000");
+                recoil = 0;
+                recoilTime = 0;
+                omniMovement = false;
+                ignoreRotation = false;
+                
+
 
                 bullet = new BasicBulletType() {{
-                    speed = 3f;
-                    damage = 15f;
-                    lifetime = 60f;
-                    width = 7f;
-                    height = 9f;
-                    frontColor = Color.valueOf("ff9966");
-                    backColor = Color.valueOf("ff6633");
+                    speed = 4;     //Speed in units/tick.
+                    damage = 15;   //Direct damage dealt on hit.
+                    lifetime = 60; //Lifetime in ticks.
+                    width = 12;
+                    height = 12;
+                    lightRadius = 22;
+                    lightOpacity = 0.5f;
+                    lightColor = Color.white;
+                    frontColor = Color.white;
+                    backColor = Color.red;
+                    trailColor = Color.red;
+                    trailLength = 10;
+                    trailWidth = 2;
+                    homingPower = 0.5f;
+                    homingRange = 100;
                 }};
             }});
             load();
