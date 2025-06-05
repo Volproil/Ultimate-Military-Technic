01cabfeab7aa8a1ea1dcc789188dcd3adc04452e
Volproil <88111301+Volproil@users.noreply.github.com>
2025-06-05 13:24:25 +0300

    Update M2Bradley.java
    исправление текстур

--- src/mindustry/content/units/M2Bradley.java
+++ src/mindustry/content/units/M2Bradley.java
@@ -1,21 +1,24 @@
-package mindustry.content.units;
+package mindustry.content.units; // Пакет соответствует пути файла из ошибки компиляции
 
 import arc.graphics.Color;
+import arc.math.geom.Rect;
 import mindustry.content.Fx;
 import mindustry.entities.bullet.BasicBulletType;
-import mindustry.entities.part.*;
+import mindustry.entities.part.*; // Изменено для включения PartMove и PartProgress
+import mindustry.entities.part.DrawPart.PartProgress;
 import mindustry.gen.Sounds;
 import mindustry.gen.UnitEntity;
 import mindustry.type.UnitType;
 import mindustry.type.Weapon;
 import mindustry.type.unit.TankUnitType;
 import mindustry.world.meta.Env;
-//TODO Исправить отображение башни и -barrrel.
+
 public class M2Bradley {
+
     public static UnitType createM2Bradley() {
         return new TankUnitType("M2Bradley") {{
             // Основные параметры
-            description = "Heavy tank";
+            description = "heavy tank";
             hitSize = 17f;
             health = 400f;
             armor = 8f;
@@ -25,18 +28,23 @@ public class M2Bradley {
             treadPullOffset = 4;
             outlineColor = Color.valueOf("2d2f39");
             omniMovement = false;
+            // layerOffset = -1f; // Удалено, так как вызывает ошибку "cannot find symbol"
 
+            // Протекторные прямоугольники
+            treadRects = new Rect[]{
+                new Rect(3, 7, 1, 2)
+            };
 
             // Окружающая среда
+            // Исправлено на envEnabled, так как это правильное имя поля в UnitType
             envEnabled = Env.terrestrial | Env.space;
             envDisabled = Env.underwater;
 
-
-
-            constructor = UnitEntity::create;
+            // Конструктор сущности юнита
+             constructor = UnitEntity::create;
 
             // Основное оружие
-            weapons.add(new Weapon("umt-java-m2bradley-weapon") {{
+            weapons.add(new Weapon("umt-java-m2bredley-weapon") {{
                 x = 0f;
                 y = 0f;
                 shootY = 17f;
@@ -50,20 +58,19 @@ public class M2Bradley {
                 targetAir = true;
                 targetGround = true;
                 ignoreRotation = true;
-
+                // followRotation = false;
+                // rotateShooting = false;
 
                 bullet = new BasicBulletType(3f, 40) {{
                     width = 7f;
                     height = 10f;
                     hitSize = 0.6f;
                     lifetime = 60f;
-                    trailLength = 10;
-                    trailWidth = 1.5f;
-                    trailColor = Color.valueOf("ff7f14");
-                    backColor = Color.valueOf("ffa621");
-                    frontColor = Color.valueOf("ff9a00");
+                    // trailMult = 0.8f; // Удалено, так как вызывает ошибку "cannot find symbol"
+                    trailColor = Color.valueOf("ff7f14ff");
+                    backColor = Color.valueOf("ffa621ff");
+                    frontColor = Color.valueOf("ff9a00ff");
                     shootEffect = Fx.shootBig;
-
 
                     collides = true;
                     collidesTiles = true;
@@ -71,13 +78,10 @@ public class M2Bradley {
                 }};
 
                 // Анимация ствола
-                parts.add(new RegionPart("umt-java-m2bredley-weapon-barrel") {{
-                    under = false;
+                parts.add(new RegionPart("-barrel") {{
+                    under = true;
                     y = 9.4f;
-                    moves.add(new PartMove() {{
-                        progress = PartProgress.recoil;
-                        y = -1.5f;
-                    }});
+                    moves.add(new PartMove(PartProgress.recoil, 0f, -1.5f, 0f));
                 }});
             }});
         }};
